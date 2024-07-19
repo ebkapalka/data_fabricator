66561a588bf092641f39667a83cfce4f5658c03f
Eric Kapalka <eric.kapalka@montana.edu>
2024-07-19 14:17:48 -0600

    Changed "example1" to "default"

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from generate_data import create_dataframe
 
 if __name__ == '__main__':
     num_rows = 1000
-    schema_name = 'example1'  # Change to the schema you want to use
+    schema_name = 'default'  # Change to the schema you want to use
     df = create_dataframe(schema_name, num_rows)
     df.to_csv(f'{schema_name}_data.csv', index=False)
     print(f"Generated {num_rows} rows of data "
