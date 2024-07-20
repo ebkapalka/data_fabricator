9277f7c4fb0b3467bacbcba750491e7735509374
Eric Kapalka <eric.kapalka@montana.edu>
2024-07-20 09:46:42 -0600

    Added advanced type hinting for reference

--- schemas/schema_default.py
+++ schemas/schema_default.py
@@ -1,9 +1,14 @@
+from typing import Callable, Union
 from faker import Faker
 import random
 
 fake = Faker()
 
-schema = {
+GeneratorFunction = Callable[[], Union[str, int, float, bool]]
+GeneratorList = list[Union[str, int, float, bool]]
+SchemaValue = Union[GeneratorFunction, GeneratorList]
+
+schema: dict[str, SchemaValue] = {
     'first_name': fake.first_name,
     'last_name': fake.last_name,
     'age': lambda: random.randint(18, 70),
